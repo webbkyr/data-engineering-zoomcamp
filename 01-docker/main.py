92a5a497ab18b5938876565105f541e2bb6ed962
Kayla Webb <kaylarwebb@gmail.com>
2026-01-10 16:41:55 +0000

    2026-01-10 setup virtual env, add pandas to pipeline

---
+++ 01-docker/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 01-docker!")
+
+
+if __name__ == "__main__":
+    main()
--- 01-docker/pipeline/pipeline.py
+++ 01-docker/pipeline/pipeline.py
@@ -1,6 +1,17 @@
 import sys
-
-print('hello pipeline')
+import pandas as pd
 
 if len(sys.argv) > 1:
-    print(sys.argv[1])
+    month = int(sys.argv[1])
+    print(f"Processing pipeline for date {month}")
+
+    df = pd.DataFrame({"day": [1,2], "num_passengers": [3,4]})
+    df['month'] = month
+    print(df.head())
+
+    df.to_parquet(f"output_{month}.parquet")
+else:
+    print(f"Month required.")
+    sys.exit(1)
+
+
