0e1155caf43cca0c5a45e98031dca0ee05597c35
Kayla Webb <kaylarwebb@gmail.com>
2026-01-16 02:41:50 +0000

    feat: terraform apply bucket and object

--- 01-docker/terraform/main.tf
+++ 01-docker/terraform/main.tf
@@ -8,6 +8,21 @@ terraform {
 }
 
 provider "google" {
-  project     = "my-project-id"
+  project     = "daring-pier-447713-j0"
   region      = "us-central1"
+}
+
+resource "google_storage_bucket" "static" {
+ name          = "daring-pier-20260115"
+ location      = "US"
+ storage_class = "STANDARD"
+
+ uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "default" {
+ name         = "test-object"
+ source       = "/workspaces/data-engineering-zoomcamp/01-docker/test/file1.txt"
+ content_type = "text/plain"
+ bucket       = google_storage_bucket.static.id
 }
